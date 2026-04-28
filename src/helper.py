bbf3a1fd9a0f31487b17ae698a261d58aec1d8fa
Fahimh007 <hasan35-1177@s.diu.edu.bd>
2026-04-29 02:23:02 +0600

    open_router api key used

--- src/helper.py
+++ src/helper.py
@@ -3,15 +3,17 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
-
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
+# client = OpenAI(api_key=OPENAI_API_KEY)
 
-client = OpenAI(api_key=OPENAI_API_KEY)
-
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key= OPENAI_API_KEY,
+)
 
 def extract_text_from_pdf(uploaded_file):
     """
@@ -46,7 +48,7 @@ def ask_openai(prompt, max_tokens=500):
     
 
     response = client.chat.completions.create(
-        model= "gpt-4o",
+        model= "~openai/gpt-mini-latest",
         messages=[
             {
                 "role": "user",
